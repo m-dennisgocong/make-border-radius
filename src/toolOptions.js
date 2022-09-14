db92efadbadcf9f1db4978ced83f81e35b1cd044
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-09-14 10:02:42 +0700

    add toolOptions to show the selected tool option

---
+++ src/toolOptions.js
@@ -0,0 +1,16 @@
+export function setUpOptions(element){
+    
+    const contents = Array.from(document.querySelectorAll('#toolbox .option'));  
+
+    element.forEach(button => button.addEventListener('click', function(){
+        // change the button background-color of selected button
+        element.forEach(button => button.classList.remove('setOn')) // clear  
+        this.classList.add('setOn') // add the setOn to the active button 
+    
+        // show the content of selected option
+        contents.forEach(content => content.classList.remove('show'))// clear first
+        // get the id of the content using the button attribute name as a reference 
+        const selectContent = document.getElementById(this.name)
+        selectContent.classList.add('show')// from display: none to display: block
+    }))  
+}
