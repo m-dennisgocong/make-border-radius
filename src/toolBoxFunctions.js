fb43e36246f50120ed32073af3f5d93bf388730a
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-09-15 13:52:25 +0700

    add the span element to display the changes

--- src/toolBoxFunctions.js
+++ src/toolBoxFunctions.js
@@ -1,15 +1,25 @@
-
 export function setUpToolBoxFunctions(element){
-    
+
+    const borderRadius = {
+        "top-left" : document.getElementById("top-left-value"),
+        "top-right" : document.getElementById("top-right-value"),
+        "bottom-right" : document.getElementById("bottom-right-value"),
+        "bottom-left" : document.getElementById("bottom-left-value")
+    }
+
     element.forEach(range => range.addEventListener('input', function(){
         if(this.name === "all-side"){
             element.forEach(range => (document.documentElement.style.setProperty(`--${range.name}`, this.value + "%")))
             element.forEach(range => {
                 range.value = this.value;
             })
+            for(const key in borderRadius){
+                borderRadius[key].innerText = this.value+"%";
+            }
+            
         }else{
-            document.documentElement.style.setProperty(`--${this.name}`, this.value + "%");         
+            document.documentElement.style.setProperty(`--${this.name}`, this.value + "%"); 
+            borderRadius[this.name].innerText = this.value+"%";         
         }
-        
     }));
 }
