bf1c79c32bf00c003b866d3635b0caf1a0866e01
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-09-16 19:53:05 +0700

    separate the background animation

---
+++ src/backgroundAnimation.js
@@ -0,0 +1,10 @@
+import './backgroundAnimation.css'
+export function setUpBackground(element){   
+    element.innerHTML = `
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    `      
+}
